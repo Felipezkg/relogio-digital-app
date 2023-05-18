3fde9757020d111b9a71f37b3bf313ef4c1f5881
Felipezkg <felipezkg@gmail.com>
2023-05-17 22:27:39 -0300

    Relógio Digital React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [horas, setHoras] = useState(0);
+  const [minutos, setMinutos] = useState(0);
+  const [segundos, setSegundos] = useState(0);
+
+  useEffect(() => {
+    const relogio = setInterval(() => {
+      const dateToday = new Date();
+      const hr = dateToday.getHours();
+      const min = dateToday.getMinutes();
+      const seg = dateToday.getSeconds();
+
+      setHoras(hr);
+      setMinutos(min);
+      setSegundos(seg);
+    }, 1000);
+
+    return () => clearInterval(relogio);
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='relogio'>
+      <div>
+        <span>{horas}</span>
+        <span className='tempo'>Horas</span>
+      </div>
+      <div>
+        <span>{minutos}</span>
+        <span className='tempo'>Minutos</span>
+      </div>
+      <div>
+        <span>{segundos}</span>
+        <span className='tempo'>Segundos</span>
+      </div>
     </div>
   );
 }
